709617af5f244a6def41003d67ae6e330049eb9b
VADDI <VADDI@LAPTOP-V7AQA1EH>
2020-07-19 16:53:08 +0530

    commit

---
+++ DataStreams/src/main/java/com/vaddi/DataStreams/App.java
@@ -0,0 +1,13 @@
+package com.vaddi.DataStreams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
